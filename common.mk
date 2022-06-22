2645690e6d44e4dd81e50ec7bba48f0cf5a47138
Menglong Dong <imagedong@tencent.com>
2022-06-22 16:57:39 +0800

    move script of drop_reason.h to common.mk
    As libbpf-based nettrace alse needs drop_reason.h, so move it
    out of droptrace.
    
    Signed-off-by: Menglong Dong <imagedong@tencent.com>

--- common.mk
+++ common.mk
@@ -7,14 +7,31 @@ HOST_CFLAGS := $(CFLAGS) -lbpf -lelf -lz -Wno-deprecated-declarations \
 SHARED = $(ROOT)/shared
 COMPONENT = $(ROOT)/component
 COMMON_SHARED = $(SHARED)/pkt_utils.c $(COMPONENT)/net_utils.c \
-		$(COMPONENT)/arg_parse.c
+		$(COMPONENT)/arg_parse.c $(COMPONENT)/sys_utils.c
 REMOTE_ROOT := https://raw.githubusercontent.com/OpenCloudOS/OpenCloudOS-Kernel/kernel5.4/master/tools/bpf/nettrace/
 export REMOTE_ROOT
-
 
 vmlinux.h:
 	$(BPFTOOL) btf dump file $(VMLINUX) \
 	format c > $@
+
+drop_reason.h: vmlinux.h
+	rm -rf $@
+	@awk 'BEGIN{ print "#ifndef _H_SKB_DROP_REASON"; \
+		print "#define _H_SKB_DROP_REASON\n";\
+		system("sed -e \"/enum skb_drop_reason {/,/}/!d\" $< >> $@");\
+		print "\n#define __DEFINE_SKB_REASON(FN) \\";\
+	}\
+	/^enum skb_drop/ { dr=1; }\
+	/^\};/ { dr=0; }\
+	/^\tSKB_DROP_REASON_/ {\
+		if (dr) {\
+			sub(/SKB_DROP_REASON_/, "", $$1);\
+			printf "\tFN(%s)\t\\\n", $$1;\
+		}\
+	}\
+	END{ print "\n#endif" }' $< >> $@
+	@echo generated drop_reason.h
 
 progs/%.o: progs/%.c vmlinux.h
 	clang -O2 -c -g -S -Wall -Wno-pointer-sign -Wno-unused-value	\
@@ -32,8 +49,10 @@ $(bpf_progs): %: %.skel.h
 bpf: $(bpf_progs)
 
 $(progs): %: %.c bpf
-	@if [ -n "$(prog-$@)" ]; then			\
-		gcc $(prog-$@) -o $@ $(HOST_CFLAGS);	\
-	else						\
-		gcc $< -o $@ $(HOST_CFLAGS);		\
+	@if [ -n "$(prog-$@)" ]; then				\
+		echo gcc $(prog-$@) -o $@ $(HOST_CFLAGS);	\
+		gcc $(prog-$@) -o $@ $(HOST_CFLAGS);		\
+	else							\
+		echo gcc $< -o $@ $(HOST_CFLAGS);		\
+		gcc $< -o $@ $(HOST_CFLAGS);			\
 	fi
