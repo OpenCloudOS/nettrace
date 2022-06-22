d39cdb413d9ffbf755d255a6abb47448a54d7df6
Menglong Dong <imagedong@tencent.com>
2022-06-22 17:56:29 +0800

    update bash-completion.sh for nettrace
    Signed-off-by: Menglong Dong <imagedong@tencent.com>

--- script/bash-completion.sh
+++ script/bash-completion.sh
@@ -1,2 +1,3 @@
-complete -W '-h -s -d --addr -p --dport --sport --port -t -v --detail --stack --stack-tracer --timeline -c --ret --skb-mode --force-stack --tcp-flags -o --output' nettrace
+complete -W '-s --saddr -d --daddr --addr -p -D --dport -S --sport -P --port -t --trace -v  --debug --ret --timeline --intel --intel-quiet --intel-keep -h --help' nettrace
+complete -W '-h -s -d --addr -p --dport --sport --port -t -v --detail --stack --stack-tracer --timeline -c --ret --skb-mode --force-stack --tcp-flags -o --output' nettrace-legacy
 complete -W '-s --saddr -d --daddr --addr -S --sport -D --dport -P --port -p --proto -r --reason --raw-sym --stat --stat-stop -l --limit --limit-budget -h --help' droptrace
