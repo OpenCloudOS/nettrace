c7676fa5f5756122c6cdf3ccb646a662d54c01c4
Menglong Dong <dongml2@chinatelecom.cn>
2026-03-05 13:55:37 +0800

    enhance the bash competion and arch PKGBUILD
    Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>

--- script/bash-completion.sh
+++ script/bash-completion.sh
@@ -1,14 +1,12 @@
-complete -W '-s --saddr -d --daddr --addr -p -D --dport -S --sport -P --port
-             --netns --netns-current --pid --min-latency --pkt-len
-             --tcp-flags --pkt-len --tcp-rtt --tcp--srtt
+complete -W '-s --saddr -d --daddr --addr -S --sport -D --dport -P --port -p --proto
+             --netns --netns-current --pid --min-latency --pkt-len --tcp-flags
 
              --basic --diag --diag-quiet --diag-keep --drop --drop-stack
-             --sock --rtt --rtt-detail --filter-srtt
-             --filter-minrtt --latency-show --latency-free --latency
-             --latency-summary
+             --sock --rtt --rtt-detail --filter-srtt --filter-minrtt
+             --latency-show --latency-free --latency --latency-summary
 
              -t --trace --force --ret --detail --date -c --count --hooks
-             --tiny-show --trace-stack --trace-matcher --trace-noclone
-             --func-stats --rate-limit --btf-path
+             --tiny-show --trace-stack --trace-matcher --trace-exclude
+             --trace-noclone --trace-free --func-stats --rate-limit --btf-path
 
              -v --debug -h --help -V --version' nettrace
